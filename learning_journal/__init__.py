b08871ea0e906395e4969c74e184b659ee0ea4bf
pvosper <pvosper@mac.com>
2017-02-21 20:38:46 -0800

    WIP: requirements

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -4,13 +4,17 @@ from .models.mymodel import Base, DBSession
 
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
+import os
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
+    if 'DATABASE_URL' in os.environ:
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+    secret = os.environ.get('AUTH_SECRET', 'somesecret')
     config = Configurator(
         settings=settings,
         authentication_policy=AuthTktAuthenticationPolicy('somesecret'),
--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -2,6 +2,12 @@ import os
 import sys
 import transaction
 
+with transaction.manager:
+    password = os.environ.get('ADMIN_PASSWORD', 'admin')
+    encrypted = password_context.encrypt(password)
+    admin = User(name=u'admin', password=encrypted)
+    DBSession.add(admin)
+
 from sqlalchemy import engine_from_config
 
 from pyramid.paster import (
@@ -40,7 +46,8 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
-
+    if 'DATABASE_URL' in os.environ:
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
